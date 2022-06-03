9879d85f1316060837cc38732fbe283759a3abf4
rafamarquesrmb <rafa.rmb@gmail.com>
2022-06-03 18:31:25 -0300

    Refactor in pets on index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
 import type { NextPage } from 'next'
 import List from '../ui/components/List.tsx/List'
 import Title from '../ui/components/Title/Title'
+import { Pet } from '../data/@types/Pet'
 
+const pets_test = 
+[
+  {
+    id: 1,
+    name: 'Bidu',
+    bio_description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ducimus architecto sit laborum praesentium inventore, officiis ullam assumenda asperiores, modi ratione quia vero voluptatem nostrum fugiat cupiditate at itaque rem.',
+    img_slug: 'https://media.gazetadopovo.com.br/viver-bem/2018/11/dog3-600x459-0e01be20.jpg',
+  },
+  {
+    id: 2,
+    name: 'Miau',
+    bio_description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ducimus architecto sit laborum praesentium inventore, officiis ullam assumenda asperiores, modi ratione quia vero voluptatem nostrum fugiat cupiditate at itaque rem.',
+    img_slug: 'https://www.granvitapet.com.br/sitedata//imgsdt/geral/gato-miando2.jpg',
+  },
+]
 
 const Home: NextPage = () => {
   return (
@@ -12,20 +28,7 @@ const Home: NextPage = () => {
           pode <strong>adotar um pet virtualmente</strong>
         </span>
       }></Title>
-      <List pets={[
-        {
-          id: 1,
-          name: 'Bidu',
-          bio_description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ducimus architecto sit laborum praesentium inventore, officiis ullam assumenda asperiores, modi ratione quia vero voluptatem nostrum fugiat cupiditate at itaque rem.',
-          img_slug: 'https://media.gazetadopovo.com.br/viver-bem/2018/11/dog3-600x459-0e01be20.jpg',
-        },
-        {
-          id: 2,
-          name: 'Miau',
-          bio_description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores ducimus architecto sit laborum praesentium inventore, officiis ullam assumenda asperiores, modi ratione quia vero voluptatem nostrum fugiat cupiditate at itaque rem.',
-          img_slug: 'https://www.granvitapet.com.br/sitedata//imgsdt/geral/gato-miando2.jpg',
-        }, ,
-      ]}></List>
+      <List pets={pets_test}></List>
     </div>
   )
 }
